e1f92cc4cb57f1500a4be98e9fb8868acaa955ce
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-30 11:49:03 -0300

    feat : Resolvendo o 5 exercicio da lista.

---
+++ Exercicio_05/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int formula,
+    contador;
+
+for (contador = 1; contador <= 10; ++contador)
+{
+    formula = n * contador;
+    Console.WriteLine(n + " X " + contador + " = " + formula);
+}
