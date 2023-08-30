82c763c3498842e05fe64e8a99aacd12cf0a373c
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-30 18:19:20 -0300

    feat : Resolvendo exercicio 6 da lista.

---
+++ Exercicio_06/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero = 0, quantPar = 0, quantImpar = 0;
+
+while (numero >=0)
+{
+    Console.WriteLine("Digite um número (numeros negativos finalizam): ");
+    numero=Convert.ToInt32(Console.ReadLine());
+    if(numero >= 0){
+        if((numero % 2)==0 ){
+            quantPar++;
+        }
+        else{
+            quantImpar++;
+        }
+    }
+}
+Console.WriteLine("\n\nQuantidade de Números Pares: "+quantPar+"\nQuantidade de Números Impares: "+quantImpar);
