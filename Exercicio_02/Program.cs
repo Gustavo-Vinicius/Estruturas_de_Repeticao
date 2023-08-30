dcb8c2e9fd8499223ff141fe747f9db3224fb708
Gustavo Vinicius Borges Rodrigues <gustavoborges@radinfo.com.br>
2023-08-30 11:05:13 -0300

    feat : Resolvendo questão 2 do exercicio.

---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite a quantidade de números: ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+long[] numeros = new long[quantidade];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i <  numeros.Length; i++)
+{
+    long resultado = 1;
+    for (long y = numeros[i]; y > 0; y--)
+    {
+        resultado = y*resultado;
+    }
+
+    Console.WriteLine("O fatorial de "+ numeros[i] + " é: " + resultado);
+}
