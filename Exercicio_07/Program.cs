bd810ea5cf94070e68840333ddaf167c5d11f338
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-31 10:48:18 -0300

    feat : Resolvendo o exercicio 7 da lista.

---
+++ Exercicio_07/Program.cs
@@ -0,0 +1,19 @@
+﻿int numero = 0,
+    resultado = 0;
+
+do
+{
+    Console.WriteLine("Digite um número maior que 0 e menor que 10: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+} while (numero <= 0 || numero >= 10);
+for (int i = numero; i < 40 + numero; i++)
+{
+    if (i % 2 == 1)
+    {
+        resultado += i * i;
+    }
+}
+Console.WriteLine(
+    "O resultado da soma dos quadrados dos 20 primeiros números inteiros positivos ímpares a partir do número informado é: "
+        + resultado
+);
