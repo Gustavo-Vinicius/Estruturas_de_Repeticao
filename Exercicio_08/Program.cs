ac71a7d5a727e9d7c3eb4fa3fa23de50d17ad177
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-31 14:15:26 -0300

    feat : Resolvendo o exercicio 8 da lista.

---
+++ Exercicio_08/Program.cs
@@ -0,0 +1,37 @@
+﻿int totalAlunos = 0;
+double somaNotas = 0;
+
+Console.WriteLine("Calculadora de Média de Notas");
+Console.WriteLine("Digite as notas dos alunos. Para sair, digite um número negativo.");
+
+while (true)
+{
+    Console.Write("Número de matrícula do aluno: ");
+    string matriculaInput = Console.ReadLine();
+
+    if (!int.TryParse(matriculaInput, out int matricula) || matricula < 0)
+    {
+        break; // Sai do loop se o usuário digitar um número negativo ou um valor inválido.
+    }
+
+    Console.Write("Nota do aluno: ");
+    if (double.TryParse(Console.ReadLine(), out double nota))
+    {
+        totalAlunos++;
+        somaNotas += nota;
+    }
+    else
+    {
+        Console.WriteLine("Nota inválida. Tente novamente.");
+    }
+}
+
+if (totalAlunos > 0)
+{
+    double media = somaNotas / totalAlunos;
+    Console.WriteLine($"Média das notas dos {totalAlunos} alunos: {media:F2}");
+}
+else
+{
+    Console.WriteLine("Nenhum aluno foi inserido.");
+}
